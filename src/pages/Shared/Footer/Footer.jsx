21dc32c91bec75b030470d886d56ef78a4eb919d
ahmed <ahmedramim821@gmail.com>
2023-04-26 12:55:49 +0600

    file structured for the news dragon

---
+++ src/pages/Shared/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            <p className='text-center'><small>copyright 2023 here</small></p>
+        </div>
+    );
+};
+
+export default Footer;
---
+++ src/pages/Shared/LeftNav/LeftNav.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LeftNav = () => {
+    return (
+        <div>
+            <h1>This is LeftNav</h1>
+        </div>
+    );
+};
+
+export default LeftNav;
